592255da6695a3070f32047173129bbde9c61b63
balu <balagopal.u@gmail.com>
2019-04-13 00:53:27 +0800

    resampling data

---
+++ 3_rebalance_rolled_data.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import helpers
+from imblearn.over_sampling import SMOTE, ADASYN
+from imblearn.under_sampling import ClusterCentroids
+from imblearn.under_sampling import NearMiss
+from imblearn.combine import SMOTEENN
+from imblearn.combine import SMOTETomek
+from imblearn.under_sampling import TomekLinks
+from imblearn.under_sampling import EditedNearestNeighbours
+
+class DataBalancer():
+	def __init__(self,seed=42):
+		self.data_src = 'data/windowed/'
+		self.dst = 'data/windowed_balanced/'
+		self.verbose = 1
+
+		self.columns = None
+		self.seed = seed
+
+	def load_data(self,split):
+		assert split in ('train','test')
+		df = pd.read_csv('%s%s.csv'%(self.data_src,split))
+		X = df[[x for x in df.columns if x not in ('serial_number','failure')]]
+		y = df['failure']
+		self.columns = X.columns
+		if self.verbose > 0:
+			print(X.shape,y.shape)
+			helpers.unique(y)
+		return X,y
+
+	def get_samplers(self,method):
+		samplers = {
+		'none':None,
+		'smote':SMOTE(random_state=self.seed,n_jobs=32),
+		'adasyn':ADASYN(random_state=self.seed,n_jobs=32),
+		'cluser':ClusterCentroids(random_state=self.seed,n_jobs=32),
+		'near_miss':NearMiss(random_state=self.seed,n_jobs=32),
+		'smoteenn':SMOTEENN(random_state=self.seed,smote=SMOTE(random_state=self.seed,n_jobs=16),enn=EditedNearestNeighbours(n_jobs=16)),
+		'smotetomek':SMOTETomek(random_state=self.seed,smote=SMOTE(random_state=self.seed,n_jobs=16),tomek=TomekLinks(n_jobs=16)),
+		}
+		return samplers[method]
+
+	def resample(self,method,split):
+		X,y = self.load_data(split=split)
+		sampler = self.get_samplers(method=method)
+		if sampler is not None:
+			X,y = sampler.fit_resample(X,y)
+		if self.verbose > 0:
+			print(method,split,X.shape,y.shape)
+			helpers.unique(y)
+		return X,y
+
+	def save_data(self,X,y,name):
+		helpers.create_folder(self.dst)
+		y = y.reshape(-1,1)
+		data = np.hstack((X,y))
+		cols = np.hstack([self.columns,'failure'])
+		df = pd.DataFrame(data=data,columns=cols)
+		df.to_csv('%s%s.csv'%(self.dst,name))
+		print(df.shape)
+		helpers.unique(df['failure'])
+
+	def rebalance_data(self,split,method):
+		X,y = self.resample(method=method,split=split)
+		self.save_data(X,y,name='%s_%s'%(method,split))
+
+if __name__ == '__main__':
+	bal = DataBalancer(seed=42)
+	bal.rebalance_data(split='train',method='smotetomek')
+	bal.rebalance_data(split='test',method='none')
